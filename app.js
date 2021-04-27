f9b236874d333ed95121eb10b2f84ee70f89b7aa
vpanameno <valerie.michelle@icloud.com>
2021-04-27 14:23:07 -0700

    Final project

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ app.use("/static", express.static("public"));
 
 const projects = data.projects;
 
+app.get("/error", (req, res, next) => {
+  // Log out custom error handler indication
+  console.log("custom error route called");
+
+  const err = new Error();
+  err.message = "Custom 500 error thrown";
+  err.status = 500;
+  next(err);
+});
+
 //setting up index route
 app.get("/", (req, res) => {
   res.render("index", { projects });
@@ -26,9 +36,34 @@ app.get("/about", (req, res) => {
 
 //dynamic project routes - render a different project template
 app.get("/project/:id", function(req, res, next) {
-  projectId = req.params.id;
-  const project = projects.find(({ id }) => id === +projectId);
-  res.render("project", { project });
+  const project = projects[req.params.id];
+  if (project) {
+    res.render("project", { project });
+  } else {
+    const err = new Error("This page is not found");
+    err.status = 404;
+    next(err);
+  }
+});
+//ERROR HANDLERS
+//404 HANDLER TO CATCH NON EXISTENT ROUTE REQUESTS
+
+app.use((req, res, next) => {
+  console.log("404 has been thrown");
+  const err = new Error("Page Not Found"); //creating error object
+  err.status = 404;
+  next(err); //error object is passed to next function
+});
+
+//GLOBAL HANDLER
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(err.status);
+    res.render("not-found", { err });
+  } else {
+    err.message = "Oops! It looks like something went wrong with the server.";
+    res.status(err.status || 500).render("error", { err });
+  }
 });
 
 //Add listener for the host
